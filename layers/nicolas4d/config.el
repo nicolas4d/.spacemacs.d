d2bc0e3495e9a1f3b592836222eaedcca44edef7
d <nicolas4d@foxmail.com>
2018-03-31 11:51:15 +0800

    edit font settings in windows

--- layers/nicolas4d/config.el
+++ layers/nicolas4d/config.el
@@ -1,5 +1,5 @@
 (if (string= system-type "windows-nt")
-    (set-default-font "-outline-幼圆-normal-normal-normal-mono-16-*-*-*-c-*-ascii-0")
+    (set-default-font "-outline-SimSun-normal-normal-normal-*-13-*-*-*-p-*-iso10646-1")
   )
 
 (delete-selection-mode t)
