dec02c010ef296af72771e4d5d95674f6733454a
nicolas4d <nicolas4d@foxmail.com>
2019-07-21 19:46:19 +0800

    projectile or counsel-git

--- layers/nicolas4d/keybindings.el
+++ layers/nicolas4d/keybindings.el
@@ -6,3 +6,4 @@
 
 (spacemacs/set-leader-keys "dll" 'nicolas4d/load-my-layout)
 (spacemacs/set-leader-keys "dls" 'nicolas4d/save-my-layout)
+(spacemacs/set-leader-keys "pf" 'nicolas4d/open-file-with-projectile-or-counsel-git)
