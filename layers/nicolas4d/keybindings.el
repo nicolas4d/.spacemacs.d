020f6b385f0b76e6c8187cb04108d9cb78c4a010
nicolas4d <nicolas4d@foxmail.com>
2019-08-08 08:21:30 +0800

    list-buffer > buffer-menu

--- layers/nicolas4d/keybindings.el
+++ layers/nicolas4d/keybindings.el
@@ -7,3 +7,7 @@
 (spacemacs/set-leader-keys "dll" 'nicolas4d/load-my-layout)
 (spacemacs/set-leader-keys "dls" 'nicolas4d/save-my-layout)
 (spacemacs/set-leader-keys "pf" 'nicolas4d/open-file-with-projectile-or-counsel-git)
+
+;;; Rebind 'C-x C-b' for 'buffer-menu'
+(global-set-key "\C-x\C-b" 'buffer-menu)
+
