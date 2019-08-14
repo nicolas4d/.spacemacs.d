5611b396deb05da2b0dd2a3d7d917cc7f9c72c52
nicolas4d <nicolas4d@foxmail.com>
2019-08-14 08:39:05 +0800

    alter layer/nicolas4d-prgram/config.el

--- layers/nicolas4d-program/funcs.el
+++ layers/nicolas4d-program/funcs.el
@@ -1,7 +1,7 @@
 ;;tags-company
-(defvar tags-company/conf
-  ".TAGS_CONF"
-  "tags-company configuration default file name")
+(defvar tags-company/tags-file-name
+  "TAGS"
+  "default tags file name.")
 
 (defvar tags-company/start-create-tags-msg
   "Creating TAGS in %s ..."
@@ -11,27 +11,12 @@
   "ctags -f %s -e -R %s"
   "create tags command and option string")
 
-(defvar tags-company/tags-file-name
-  "TAGS"
-  "default tags file name.")
-
 (defvar tags-company/set-environment-msg
   "set tags-company environment succeed")
 (defvar tags-company/unset-environment-succeed-msg
   "unset tags-company environment succeed")
 (defvar tags-company/unset-environment-not-found-msg
   "not found tags-company environment")
-
-(defvar tags-company/update-all-tags
-  nil
-  "whether create all tags-company tags.
-default nil. just create tags for current tags-company.
-if t, create tags for all tags-company in the tags-table-list."
-  )
-
-(defvar tags-company/current-conf-directory
-  ""
-  "current tags-company directory that contains configuration.")
 
 (defun tags-company/find-environment()
   "locate tags company root directory.
@@ -40,12 +25,11 @@ find directory that contain tags-company-conf."
         (conf-directory))
     (setq conf-directory
           (locate-dominating-file directory tags-company/conf))
-    (setq tags-company/current-conf-directory conf-directory)
     conf-directory))
 
 (defun tags-company/read-conf(file)
   "read configuration file's contents.
-return the file contents."
+return the file contents as list."
   (save-excursion
     (let ((buffer-string))
       (set-buffer (find-file-noselect file))
@@ -55,20 +39,14 @@ return the file contents."
 
 (defun tags-company/set-up-company ()
   "make use of custom tags file for company-mode"
-  (let ((conf-file (tags-company/find-environment))
+  (let ((conf-directory (tags-company/find-environment))
         conf-list)
-    (if conf-file
+    (if conf-directory
         (progn
-          (setq conf-list
-                (tags-company/read-conf (concat conf-file tags-company/conf)))
+          (setq conf-list (tags-company/read-conf (concat conf-directory tags-company/conf)))
           (if (equal conf-list '(""))
-              (setq tags-table-list
-                    (list conf-file))
-          (setq tags-table-list
-                (list
-                 conf-file
-                 (tags-company/read-conf
-                  (concat conf-file tags-company/conf))))))
+              (setq tags-table-list (list conf-directory))
+            (setq tags-table-list (cons conf-directory conf-list))))
       (setq tags-table-list nil))))
 
 (defun tags-company/create-tags (SRC-DIR &optional FORCE)
@@ -91,14 +69,15 @@ command use create-tags-command-and-option."
   "check the tags in tags-table-list and re-create it."
   (interactive)
   (tags-company/set-up-company)
-  (if tags-company/update-all-tags
-      (dolist (tags-file tags-table-list)
+  (when tags-table-list
+    (if tags-company/update-all-tags
+        (dolist (tags-file tags-table-list)
+          (make-thread
+           (tags-company/create-tags (file-name-directory tags-file) t)))
+      (progn
+        (setq tags-file (car tags-table-list))
         (make-thread
-         (tags-company/create-tags (file-name-directory tags-file) t)))
-    (progn
-      (setq tags-file (car tags-table-list))
-      (make-thread
-       (tags-company/create-tags (file-name-directory tags-file) t)))))
+         (tags-company/create-tags (file-name-directory tags-file) t))))))
 
 (defun tags-company/set-environment()
   "create tags-company/conf configuration file."
