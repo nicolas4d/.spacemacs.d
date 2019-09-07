960aeb9e4f4489dfa31cefcd57868eccb661d032
nicolas4d <nicolas4d@foxmail.com>
2019-09-07 16:07:20 +0800

    evil normal state map

--- layers/nicolas4d-defaults/keybindings.el
+++ layers/nicolas4d-defaults/keybindings.el
@@ -21,4 +21,8 @@
 (define-key evil-motion-state-map (kbd "C-d") nil)
 (define-key evil-motion-state-map (kbd "C-f") nil)
 (define-key evil-motion-state-map (kbd "C-y") nil)
+
+(define-key evil-normal-state-map (kbd "q") nil)
+(define-key evil-normal-state-map (kbd "C-p") nil)
+(define-key evil-normal-state-map (kbd "C-n") nil)
 ;;; evil ends here
